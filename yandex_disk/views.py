7a4f880a9dd1dffa8b4b873c90b2d54b9adaee8f
S1im_Sh4dy <mihakurd@gmail.com>
2024-09-10 22:14:06 +0300

    Refactoring views.py

--- yandex_disk/views.py
+++ yandex_disk/views.py
@@ -4,15 +4,17 @@ from django.shortcuts import render
 from requests import HTTPError
 from django.core.cache import cache
 
+TEMPLATE_FILES_LIST = 'files_list.html'
+
 def files_list(request) -> HttpResponse:
     public_key = request.GET.get('public_key')
     if not public_key:
-        return render(request, 'files_list.html', {'error': 'Данные не введены'})
+        return render(request, TEMPLATE_FILES_LIST, {'error': 'Данные не введены'})
 
     cache_key = f'file_list_{public_key}'
     cached_data = cache.get(cache_key)
     if cached_data:
-        return render(request, 'files_list.html', {'items': cached_data})
+        return render(request, TEMPLATE_FILES_LIST, {'items': cached_data})
 
     url = f'https://cloud-api.yandex.net/v1/disk/public/resources'
     params = {'public_key': public_key}
@@ -20,13 +22,13 @@ def files_list(request) -> HttpResponse:
     try:
         response.raise_for_status()
     except HTTPError:
-        return render(request, 'files_list.html', {'error': 'Невозможно получить файлы'})
+        return render(request, TEMPLATE_FILES_LIST, {'error': 'Невозможно получить файлы'})
 
     data = response.json()
     items = [data] if data.get('type') == 'file' else data['_embedded']['items']
 
     cache.set(cache_key, items, timeout=600)
-    return render(request, 'files_list.html', {'items': items})
+    return render(request, TEMPLATE_FILES_LIST, {'items': items})
 
 
 
